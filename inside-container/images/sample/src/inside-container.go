f35017657a8ede16482199b9b234e9fced3f7608
Raj Chaudhuri <rajch@hotmail.com>
2018-05-15 10:44:04 +0530

    Modified the inside-container application to show environment variables and files

--- inside-container/images/sample/src/inside-container.go
+++ inside-container/images/sample/src/inside-container.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
-)
-
-const (
-	htmlStartToTitle = "<!doctype html><html><head><title>"
-	htmlTitleToBody  = "</title></head><body>"
-	htmlBodyToEnd    = "</body></html>"
-	fmtOneInfoLine   = "<div><label>%s: </label><span>%s</span></div>"
+	"path"
+	"strconv"
+	"strings"
 )
 
 func getHostName() string {
@@ -40,17 +37,76 @@ func getIPAddresses() string {
 	return result
 }
 
+func getEnvironmentVariables() string {
+	result := ""
+	env := os.Environ()
+
+	for _, variable := range env {
+		result += variable + "\n"
+	}
+
+	return result
+}
+
+func getFiles(startpath string, depth int, currentdepth int) string {
+	var result string
+
+	entries, err := ioutil.ReadDir(startpath)
+	if err != nil {
+		result = err.Error()
+		return result
+	}
+
+	for _, entry := range entries {
+		if currentdepth > 0 {
+			result += strings.Repeat("   ", currentdepth)
+
+			if entry.IsDir() {
+				result += "\\"
+			} else {
+				result += "|"
+			}
+
+			result += "--"
+		}
+
+		result += entry.Name() + "\n"
+
+		if entry.IsDir() && currentdepth < depth {
+			result += getFiles(path.Join(startpath, entry.Name()), depth, currentdepth+1)
+		}
+	}
+
+	return result
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request received.")
 
-	fmt.Fprint(w, htmlStartToTitle)
-	fmt.Fprint(w, "Inside Container")
-	fmt.Fprint(w, htmlTitleToBody)
-	fmt.Fprintf(w, fmtOneInfoLine, "Hostname", getHostName())
-	fmt.Fprintf(w, fmtOneInfoLine, "IP Addresses", getIPAddresses())
-	fmt.Fprintf(w, htmlBodyToEnd)
+	fmt.Fprintln(w, "Hostname: "+getHostName())
+	fmt.Fprintln(w, "IP Addresses: ", getIPAddresses())
 
 	fmt.Println("Request concluded.")
+}
+
+func envHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, getEnvironmentVariables())
+}
+
+func filesHandler(w http.ResponseWriter, r *http.Request) {
+
+	startpath := strings.TrimPrefix(r.URL.Path, "/files")
+	if startpath == "" {
+		startpath = "/"
+	}
+
+	depthqs := r.URL.Query()["depth"]
+	depth := 1
+	if len(depthqs) > 0 {
+		depth, _ = strconv.Atoi(depthqs[0])
+	}
+
+	fmt.Fprintln(w, getFiles(startpath, depth, 0))
 }
 
 func main() {
@@ -61,6 +117,8 @@ func main() {
 		portNumber = ":" + portNumber
 	}
 
+	http.HandleFunc("/env", envHandler)
+	http.HandleFunc("/files/", filesHandler)
 	http.HandleFunc("/", rootHandler)
 
 	fmt.Println("Hostname:" + getHostName())
